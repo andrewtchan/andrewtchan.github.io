6093992bce4b5b546cd7376e28ed210d46a583ff
Andrew Chan <andrew2020chan@gmail.com>
2025-04-22 23:09:30 -0700

    feat: basic image config

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,18 @@
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    extensions: 'html',
+    formats: ['webp', "jpeg"],
+    widths: ["auto"],
+    htmlOptions: {
+      imgAttributes: {
+        loading: "lazy",
+        decoding: "async",
+        sizes: "auto",
+      },
+      pictureAttributes: {}
+    },
+  });
 };
