a9440b9d43397514538deff0f7dd858a5b7b5fa4
Andrew Chan <andrew2020chan@gmail.com>
2025-05-15 13:14:50 -0700

    perf: try eager loading images

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function (eleventyConfig) {
     widths: ["auto"],
     htmlOptions: {
       imgAttributes: {
-        loading: "lazy",
+        loading: "eager",
         decoding: "async",
         sizes: "auto",
       },
